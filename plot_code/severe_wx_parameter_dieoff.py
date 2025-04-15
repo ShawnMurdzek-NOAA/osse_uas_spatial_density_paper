471bf02f3057fa2febf0338c9a5ffb7f491fc282
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2025-04-14 21:24:51 -0500

    Change severe wx environment plot so diffs can be more easily seen

--- plot_code/severe_wx_parameter_dieoff.py
+++ plot_code/severe_wx_parameter_dieoff.py
@@ -27,9 +27,9 @@ yml_fname = 'verif_sim_info.yml'
 valid_times = [dt.datetime(2022, 4, 29, 21) + dt.timedelta(hours=i) for i in range(159)]
 
 # Y-axis label
-ylabel = {'MLCAPE': 'MLCAPE (J kg$^{-1}$)',
-          'MLCIN': 'MLCIN (J kg$^{-1}$)',
-          'SRH03': '0$-$3 km SRH (m$^{2}$ s$^{-2}$)'}
+ylabel = {'MLCAPE': 'MLCAPE diff (J kg$^{-1}$)',
+          'MLCIN': 'MLCIN diff (J kg$^{-1}$)',
+          'SRH03': '0$-$3 km SRH diff (m$^{2}$ s$^{-2}$)'}
 
 # Output file
 out_fname = '../figs/SevereWxDieoff.pdf'
@@ -50,36 +50,81 @@ for key in param['sim_dict_spring'].keys():
 plot_dict = param['severe_wx_spring']['severe_wx_env']
 
 # Make plot
-fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(6, 10), sharex=True)
-plt.subplots_adjust(left=0.14, bottom=0.07, right=0.98, top=0.95, hspace=0.08)
-letters = ['a', 'b', 'c']
-for i, ax in enumerate(axes):
-    v = list(plot_dict.keys())[i]
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(6, 6), sharex=True)
+plt.subplots_adjust(left=0.12, bottom=0.29, right=0.99, top=0.93, hspace=0.08, wspace=0.35)
+letters = ['a', 'b', 'c', 'd']
+for i, (v, c, ls) in enumerate(zip(list(plot_dict.keys()),
+                                   ['k', 'gray', 'saddlebrown'],
+                                   ['-', '-.', ':'])):
     print(f'Making plot for {v}')
     input_sims = copy.deepcopy(sim_dict)
     for key in input_sims:
         input_sims[key]['dir'] = input_sims[key]['dir'].format(typ='GridStat', subtyp='severe_wx_env')
+
+    # Control run
+    ctrl_sim = {}
+    if v == 'SRH03':
+        name = "ctrl 0$-$3 km SRH"
+    else:
+        name = f"ctrl {v}"
+    ctrl_sim[name] = input_sims['ctrl']
+    ctrl_sim[name]['color'] = c
+    ctrl_sim[name]['ls'] = ls
+    _ = mp.plot_sfc_dieoff(ctrl_sim, 
+                           valid_times,
+                           fcst_lead=[0, 1, 2, 3, 6, 12],
+                           plot_stat=plot_dict[v]['plot_stat'][0],
+                           ax=axes[0, 0],
+                           verbose=False,
+                           **plot_dict[v]['kwargs'])
+
+    # Differences
     _ = mp.plot_sfc_dieoff(input_sims, 
                            valid_times,
                            fcst_lead=[0, 1, 2, 3, 6, 12],
                            plot_stat=plot_dict[v]['plot_stat'][0],
-                           ax=ax,
+                           ax=axes[int((i+1) / 2), (i+1) % 2],
                            verbose=False,
+                           diffs=True,
+                           include_ctrl=False,
+                           include_zero=True,
                            **plot_dict[v]['kwargs'])
 
-    if i == 0:
-        ax.legend(ncols=2, fontsize=12)
-    else:
-        ax.get_legend().remove()
-    ax.tick_params(axis='both', which='major', labelsize=11)
+# Formatting
+for i in range(4):
+    ax = axes[int(i/2), i%2]
+
+    # Subplot labels
     ax.set_title('')
-    ax.set_ylabel(ylabel[v], size=14)
-    if i != 2:
-        ax.set_xlabel('')
-    ax.text(0.02, 0.92, f'{letters[i]})', size=14, weight='bold', transform=ax.transAxes, 
+    ax.text(0.04, 0.885, f'{letters[i]})', size=12, weight='bold', transform=ax.transAxes,
             backgroundcolor='white')
 
-plt.suptitle('RMSEs for gridpoints with MUCAPE > 50 J kg$^{-1}$', size=17)
+    # Legend
+    if i == 0:
+        ax.legend(ncols=3, fontsize=12, loc=(-0.1, -1.5))
+    elif i == 1:
+        ax.legend(ncols=2, fontsize=12, loc=(-1.15, -2))
+    else:
+        ax.get_legend().remove()
+
+    # X label
+    if i > 1:
+        ax.set_xlabel('lead time (hr)', size=12)
+    else:
+        ax.set_xlabel('')
+
+    # Y label
+    if i == 0:
+        ax.set_ylabel('J kg$^{-1}$', size=12)
+    else:
+        ax.set_ylabel(ylabel[list(plot_dict.keys())[i-1]], size=12)
+
+    # Axes limits
+    if i == 1: ax.set_ylim(top=40)
+    if i == 2: ax.set_ylim(top=5)
+    if i == 3: ax.set_ylim(top=8)
+
+plt.suptitle('RMSEs for gridpoints with MUCAPE > 50 J kg$^{-1}$', size=16)
 plt.savefig(out_fname)
 
 
