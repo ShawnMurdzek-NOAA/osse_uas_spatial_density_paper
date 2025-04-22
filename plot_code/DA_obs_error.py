dbd191f31f08641ab4941f1a8dfd51e4658bb48b
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2025-04-22 16:19:11 -0500

    Change font sizes in DA_obs_error.py

--- plot_code/DA_obs_error.py
+++ plot_code/DA_obs_error.py
@@ -41,21 +41,22 @@ out_fname = '../figs/DAerrUAS.pdf'
 errtable = gsi.read_errtable(errtable_fname)
 
 # Create plot
-fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(3, 6), sharey=True)
-plt.subplots_adjust(left=0.23, bottom=0.08, right=0.98, top=0.91, hspace=0.38)
+fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(3, 5), sharey=True)
+plt.subplots_adjust(left=0.2, bottom=0.09, right=0.98, top=0.91, hspace=0.4)
 letters = ['a', 'b', 'c']
 for ax, err, l in zip(axes, list(param.keys()), letters):
     print(f'Making plot for {err}')
     typ = param[err]['TYP']
     ax.plot(errtable[typ][err] * param[err]['scale'], errtable[typ]['prs'], 'b-', lw=2)
     ax.set_ylim([1000, 100])
-    ax.set_xlabel(param[err]['xlabel'], size=12)
-    ax.set_ylabel('pressure (hPa)', size=12)
+    ax.set_xlabel(param[err]['xlabel'], size=10)
+    ax.set_ylabel('pressure (hPa)', size=10)
     ax.grid()
-    ax.text(0.04, 0.86, f'{l})', size=10, weight='bold', transform=ax.transAxes,
+    ax.tick_params(axis='both', which='major', labelsize=8)
+    ax.text(0.9, 0.09, f'{l})', size=10, weight='bold', transform=ax.transAxes,
             backgroundcolor='white')
 
-plt.suptitle('UAS DA Observation Error\nStandard Deviations', size=14)
+plt.suptitle('UAS DA Observation Error\nStandard Deviations', size=12)
 plt.savefig(out_fname)
 plt.close()
 
